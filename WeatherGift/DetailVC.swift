5c9de84bc3ce349525ee939627cd779aaf0487dd
Kyle Burns <kyleburns@kyles-macbook-pro-3.local>
2019-03-24 19:17:16 -0400

    2.23

--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -9,13 +9,23 @@
 import UIKit
 import CoreLocation
 
+private let dateFormatter = DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "EEE, MM dd, y"
+    return dateFormatter
+}()
+
+
 class DetailVC: UIViewController {
+    
     
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var currentImage: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var currentPage = 0
     var locationsArray = [WeatherLocation]()
@@ -24,6 +34,10 @@ class DetailVC: UIViewController {
     override func viewDidLoad() {
     
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
         if currentPage != 0 {
             self.locationsArray[currentPage].getWeather {
                 self.updateUserInterface()
@@ -37,13 +51,26 @@ class DetailVC: UIViewController {
         }
     }
     func updateUserInterface() {
-        locationLabel.text = locationsArray[currentPage].name
-        dateLabel.text = locationsArray[currentPage].coordinates
-        temperatureLabel.text = locationsArray[currentPage].currentTemp
-       summaryLabel.text = locationsArray[currentPage].currentTemp
-        currentImage.image = UIImage(named: locationsArray[currentPage].currentTemp)
+        let location = locationsArray[currentPage]
+        locationLabel.text = location.name
+//        let dateString = formatTimeForTimeZone(unixDate: location.currentTime, timeZone: location.timeZone)
+        let dateString = location.currentTime.format(timeZone: location.timeZone, dateFormatter: dateFormatter)
+        dateLabel.text = location.coordinates
+        temperatureLabel.text = location.currentTemp
+        summaryLabel.text = location.currentTemp
+        currentImage.image = UIImage(named: location.currentTemp)
+        tableView.reloadData()
+        
         
     }
+    
+//    func formatTimeForTimeZone(unixDate: TimeInterval, timeZone: String) -> String {
+//        let usebaleDate = Date(timeIntervalSince1970: unixDate)
+//        dateFormatter.timeZone = TimeZone(identifier:  timeZone )
+//        let dateString = dateFormatter.string(from: usebaleDate)
+//        return dateString
+//
+//    }
     
 }
 extension DetailVC: CLLocationManagerDelegate {
@@ -96,5 +123,38 @@ extension DetailVC: CLLocationManagerDelegate {
     }
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Failed to get user location")
+    }
+}
+
+extension DetailVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locationsArray[currentPage].dailyForecastArray.count
+        
+        
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DayWeatherCell", for: indexPath)
+        as! DayWeatherCell
+        let dailyForecast = locationsArray[currentPage].dailyForecastArray[indexPath.row]
+        let timeZone = locationsArray[currentPage].timeZone
+        cell.update(with: dailyForecast, timeZone: timeZone)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+        
+    }
+}
+extension DetailVC: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 24
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let hourlyCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HourlyCell", for: indexPath)
+        return hourlyCell
     }
 }
--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -11,13 +11,31 @@ import Alamofire
 import SwiftyJSON
 
 class WeatherLocation {
+    
+    struct DailyForecast {
+        var dailyMaxTemp: Double
+        var dailyMinTemp: Double
+        var dailyDate: Double
+        var dailySummary: String
+        var dailyIcon: String
+        
+    }
+    
     var name = ""
     var coordinates = ""
     var currentTemp = "--"
+    var dailySummary = ""
+    var currentIcon = ""
+    var currentTime = 0.0
+    var timeZone = ""
+    var dailyForecastArray = [DailyForecast]()
+    
+    
+    
     func getWeather(completed: @escaping() -> ()) {
         let weatherURL = urlBase + urlAPIKey + coordinates
-        Alamofire.request(weatherURL).responseJSON { response in
-            switch.response.result {
+        AlamoFire.request(weatherURL).responseJSON { response in
+            switch response.result {
             case.success(let value):
                 let json = JSON(value)
                 if let temperature = json["currently"]["temperature"].double {
@@ -27,7 +45,38 @@ class WeatherLocation {
                 } else {
                     print("Could get the temperature")
                 }
-                print("***** json: \(json)")
+                if let summary = json["daily"]["summary"].string {
+                    self.currentIcon = icon
+                } else {
+                    print("could not return a temperature")
+                }
+                if let icon = json["currently"]["icon"].string {
+                    self.currentIcon = icon
+                }else{
+                    print("could not return an icon")
+                }
+                if let icon = json["timezone"].string {
+                    self.timeZone = timeZone
+                }else{
+                    print("could not return a timezone")
+                }
+                if let icon = json["currently"]["time"].double {
+                    self.currentTime = time
+                }else{
+                    print("could not return a time")
+                }
+                let dailyDataArray = json["daily"]["data"]
+                self.dailyForecastArray = [] 
+                for day in 1 ... dailyDataArray.count-1{
+                    let maxTemp = json["daily"]["data"][day]["tempatureHigh"].doubleValue
+                    let minTemp = json["daily"]["data"][day]["tempatureLow"].doubleValue
+                    let dateValue = json["daily"]["data"][day]["time"].doubleValue
+                    let icon = json["daily"]["data"][day]["icon"].stringValue
+                    let dailySummary = json["daily"]["data"][day]["summary"].stringValue
+                    let newDailyForecast = DailyForecast(dailyMaxTemp: maxTemp, dailyMinTemp: minTemp, dailyDate: dateValue, dailySummary: dailySummary, dailyIcon: icon)
+                    self.dailyForecastArray.append(newDailyForecast)
+                    
+                }
             case.failure(let error):
                 print(error)
             }
